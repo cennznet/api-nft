284c8e715a96a141492b5218dd63054dc5199f23
aidan-starke <starke.aidan@gmail.com>
2022-05-10 13:33:07 +1200

    update `fastify-mongodb` -> `@fastify/mongodb`

--- src/api/mongo.ts
+++ src/api/mongo.ts
@@ -1,7 +1,7 @@
 import fastifyPlugin from "fastify-plugin";
-import fastifyMongo from "fastify-mongodb";
+import fastifyMongo from "@fastify/mongodb";
 
-export default async function mongoConnector(fastify, options) {
+export default async function mongoConnector(fastify, _options) {
 	const connectionStr = process.env.MONGO_URI;
 	fastify.log.info(`connecting to mongodb ${connectionStr}`);
 	fastify.register(fastifyMongo, {
