265087abc8e50b34cfd0aff6ca0b3cb98477261e
KarishmaBothara <bothara.karishma@gmail.com>
2022-05-25 10:22:03 +1200

    get all blocknumber with batch utility (#24)

--- src/scanner/utils/fetchNFTBlockNumberForRange.ts
+++ src/scanner/utils/fetchNFTBlockNumberForRange.ts
@@ -1,21 +1,46 @@
 import axios from "axios";
+import {logger} from "@/src/logger";
 
 export async function fetchNFTBlockFromUncoverForRange(start, end) {
 	let startDate = new Date(start);
 	let endDate = new Date(end);
 	let page = 0;
-	let blockNumbers = [];
+	let nftBlockNumbers = [];
+	let batchTxBlockNumbers = [];
 	let globalBlockNumbers = [];
-	do {
-		const dateTimeInParts = startDate.toISOString().split("T");
-		const startDateStr = dateTimeInParts[0];
-		const endDateStr = endDate.toISOString().split("T")?.[0];
-		const url = `${process.env.UNCOVER_ENDPOINT}/blocks_by_section?section=nft&method=AuctionSold&startDate=${startDateStr}&endDate=${endDateStr}&page=${page}&row=100`;
-		console.log("url:", url);
-		const response = await axios.get(url);
-		blockNumbers = response?.data?.data;
-		page = page + 1;
-		globalBlockNumbers = globalBlockNumbers.concat(blockNumbers);
-	} while (blockNumbers.length > 0);
-	return globalBlockNumbers;
+	let moreNFTsExists = true;
+	let moreUtilityBatchExists = true;
+	try {
+		do {
+			const dateTimeInParts = startDate.toISOString().split("T");
+			const startDateStr = dateTimeInParts[0];
+			const endDateStr = endDate.toISOString().split("T")?.[0];
+			const urlWithNFT = `${process.env.UNCOVER_ENDPOINT}/blocks_by_section?section=nft&method=AuctionSold&startDate=${startDateStr}&endDate=${endDateStr}&page=${page}&row=100`;
+			const urlWithBatch = `${process.env.UNCOVER_ENDPOINT}/blocks_by_section?section=utility&startDate=${startDateStr}&endDate=${endDateStr}&page=${page}&row=100`;
+			let nftResponse;
+			let utilityResponse;
+			if (moreNFTsExists && moreUtilityBatchExists) {
+				[nftResponse, utilityResponse] = await Promise.all([axios.get(urlWithNFT), axios.get(urlWithBatch)]);
+			} else if (moreNFTsExists) {
+				nftResponse = await axios.get(urlWithNFT);
+			} else if (moreUtilityBatchExists) {
+				utilityResponse = await axios.get(urlWithBatch);
+			}
+			nftBlockNumbers = nftResponse?.data?.data;
+			batchTxBlockNumbers = utilityResponse?.data?.data;
+			moreNFTsExists = nftBlockNumbers && nftBlockNumbers.length > 0 ? true : false;
+			moreUtilityBatchExists = batchTxBlockNumbers && batchTxBlockNumbers.length > 0 ? true : false;
+			page = page + 1;
+			if (moreNFTsExists) {
+				globalBlockNumbers = globalBlockNumbers.concat(nftBlockNumbers);
+			}
+			if (moreUtilityBatchExists) {
+				globalBlockNumbers = globalBlockNumbers.concat(batchTxBlockNumbers);
+			}
+		} while (moreNFTsExists || moreUtilityBatchExists);
+	} catch (error) {
+		logger.error(error);
+	}
+	const uniqueBlockNumbers = Array.from(new Set(globalBlockNumbers));
+	return uniqueBlockNumbers;
 }
